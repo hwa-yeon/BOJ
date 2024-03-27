bb702fc85e7a61388d8245111ee4e92c522af6e8
hwa-yeon <74399187+hwa-yeon@users.noreply.github.com>
2024-03-27 16:01:59 +0900

    Create 1074.cpp

---
+++ 1074.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int func(int n, int r, int c) {
+	if (n == 0) return 0;
+
+	int half = pow(2, n - 1);
+	if (r < half && c < half) return func(n - 1, r, c);
+	else if (r < half && c >= half) return half * half + func(n - 1, r, c - half);
+	else if (r >= half && c < half) return 2 * half * half + func(n - 1, r - half, c);
+	else if (r >= half && c >= half) return 3 * half * half + func(n - 1, r - half, c - half);
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, r, c;
+	cin >> N >> r >> c;
+
+	cout << func(N, r, c) << "\n";
+
+	return 0;
+}

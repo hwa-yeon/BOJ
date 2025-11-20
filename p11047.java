7df4420b159f11d81152babc1e2fdbb447499db0
hwa-yeon <74399187+hwa-yeon@users.noreply.github.com>
2025-11-20 18:29:38 +0900

    Add files via upload

---
+++ p11047.java
@@ -0,0 +1,28 @@
+package boj1;
+
+import java.util.Scanner;
+
+public class p11047 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] A = new int[N];
+		for(int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		int count = 0;
+		for(int i = N - 1; i >= 0; i--) {
+			while(K >= A[i]) {
+				count++;
+				K -= A[i];
+			}
+		}
+		System.out.print(count);
+	}
+
+}
